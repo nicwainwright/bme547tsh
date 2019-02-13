4d820225da68ec85e28b027f41f020079560c3f4
Nic Wainwright <wainwrightnic@gmail.com>
2019-02-13 03:02:21 -0500

    added a test_isNormal test to test for normal thyroid function

--- test_tsh.py
+++ test_tsh.py
@@ -18,3 +18,12 @@ def test_isHypo(tshList):
                                        2.1, 3.2, 1.6, 0.1])])
 def test_isHyper(tshList):
     assert getDiagnosis(tshList) == "hyperthyroidism"
+
+
+# normal thyroid function is defined by TSH values <1 and >4 (noninclusive)
+@pytest.mark.parametrize("tshList", [([3.5, 1.6, 2.1, 1.0]), ([3]),
+                                     ([4.0, 4.0, 4.0, 1.0]),
+                                     ([2.1, 2.9, 3.5, 4.0, 1.0,
+                                       2.12354, 3.2, 1.6, 1.111])])
+def test_isNormal(tshList):
+    assert getDiagnosis(tshList) == "normal thyroid function"
