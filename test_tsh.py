c1f9640f2963eae6f4cdb7e310acc5431ce64359
Nic Wainwright <wainwrightnic@gmail.com>
2019-02-13 03:00:24 -0500

    added a test_isHyper() to test values less than 1 inside TSH lists

--- test_tsh.py
+++ test_tsh.py
@@ -9,3 +9,12 @@ import pytest
                                        7.1, 7.2, 7.6])])
 def test_isHypo(tshList):
     assert getDiagnosis(tshList) == "hypothyroidism"
+
+
+# hyperthyroidism is defined by TSH values less than 1 (noninclusive)
+@pytest.mark.parametrize("tshList", [([3.5, 0.6, 2.1, 1.0]), ([.100]),
+                                     ([4.0, 4.0, 4.0, .9]),
+                                     ([2.1, 2.9, 3.5, 4.0, -1.0,
+                                       2.1, 3.2, 1.6, 0.1])])
+def test_isHyper(tshList):
+    assert getDiagnosis(tshList) == "hyperthyroidism"
