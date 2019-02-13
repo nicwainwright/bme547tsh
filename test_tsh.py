591a8b650a9c18d0fca39c04ce20d47f4664e30d
Nic Wainwright <wainwrightnic@gmail.com>
2019-02-13 02:47:28 -0500

    pep8 all fixed and isHypo() test passes for every input. this is a grouping of  tsh values that should return a diagnosis of hypothyroidism

--- test_tsh.py
+++ test_tsh.py
@@ -4,8 +4,8 @@ import pytest
 
 # hypothyroidism is defined by TSH values greater than 4 (noninclusive)
 @pytest.mark.parametrize("tshList", [([3.5, 4.6, 2.1, 1.0]), ([100]),
-                                    ([4.0, 4.0, 4.0, 4.000001]),
-                                    ([2.1, 2.9, 3.5, 4.1, 6.3, 6.3, 6.7, 6.9,
-                                      7.1, 7.2, 7.6])])
+                                     ([4.0, 4.0, 4.0, 4.000001]),
+                                     ([2.1, 2.9, 3.5, 4.1, 6.3, 6.3, 6.7, 6.9,
+                                       7.1, 7.2, 7.6])])
 def test_isHypo(tshList):
     assert getDiagnosis(tshList) == "hypothyroidism"
