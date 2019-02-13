55cdfbc5f449589fc8e0d80338c0f581d83702aa
Nic Wainwright <wainwrightnic@gmail.com>
2019-02-13 02:43:32 -0500

    fixed pep8 style

--- test_tsh.py
+++ test_tsh.py
@@ -3,9 +3,9 @@ import pytest
 
 
 # hypothyroidism is defined by TSH values greater than 4 (noninclusive)
-@pytest.mark.parametrize("tshList",[([3.5, 4.6, 2.1, 1.0]), ([100]),
+@pytest.mark.parametrize("tshList", [([3.5, 4.6, 2.1, 1.0]), ([100]),
                                     ([4.0, 4.0, 4.0, 4.000001]),
                                     ([2.1, 2.9, 3.5, 4.1, 6.3, 6.3, 6.7, 6.9,
                                       7.1, 7.2, 7.6])])
-def isHypo(tshList):
+def test_isHypo(tshList):
     assert getDiagnosis(tshList) == "hypothyroidism"
